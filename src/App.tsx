816e1b1db6ef78bb2e1f895976d107f36930d9de
cauegraciolip <cauegracioli@outlook.com>
2022-04-07 17:09:14 -0300

    testing new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { Button } from "./components/Button";
+import { Texts } from "./components/Texts";
+import { Input } from "./components/Input";
+import { InputControl } from "./components/Input/InputControl";
+import { FaEnvelope } from "react-icons/fa";
+import { FaUserAlt } from "react-icons/fa";
+
 
 function App() {
   return (
-    <>Teste</>
+    <>
+      <Button variant="pattern">Botão</Button>
+      <Texts as="h2" fonte="subtitle">Texto</Texts>
+      <InputControl>
+        <FaEnvelope />
+        <Input placeholder="E-mail"/>
+      </InputControl>
+      <InputControl>
+        <FaUserAlt />
+        <Input placeholder="Usuário"/>
+      </InputControl>
+    </>
   )
 }
 
